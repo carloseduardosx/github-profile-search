936d5b194d5eca818aa1eb01f502b208b83bcd90
carloseduardosx <carloseduardosx2015@gmail.com>
2017-08-28 18:41:47 -0300

    Add webpack prod configuration
    - For now, webpack prod configuration should be equal to dev
    configuration

---
+++ webpack.config.prod.babel.js
@@ -0,0 +1,97 @@
+import autoprefixer from 'autoprefixer';
+import path from 'path';
+import webpack from 'webpack';
+import DashboardPlugin from 'webpack-dashboard/plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import HtmlWebpackHarddiskPlugin from 'html-webpack-harddisk-plugin';
+
+const htmlPath = path.resolve(__dirname, 'public/index.html');
+const appPath = path.resolve(__dirname, 'src/index.jsx');
+const buildPath = path.resolve(__dirname, 'dist');
+
+const config = {
+  devtool: 'eval-source-map',
+  entry: {
+    app: appPath
+  },
+  output: {
+    path: buildPath,
+    filename: '[name].bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    strictExportPresence: true,
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            presets: [
+              'es2015',
+              'react'
+            ],
+            plugins: [
+              ['transform-decorators-legacy'],
+              ['transform-react-jsx', { pragma: 'h' }]
+            ]
+          },
+        }]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg|ico)$/,
+        loader: 'url-loader?limit=100000'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                require('postcss-flexbugs-fixes'),
+                autoprefixer({
+                  browsers: [
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 9',
+                  ],
+                  flexbox: 'no-2009',
+                }),
+              ],
+            },
+          },
+          'sass-loader'
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new DashboardPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: htmlPath,
+    }),
+    new HtmlWebpackHarddiskPlugin()
+  ]
+};
+
+export default config;

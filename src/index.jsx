982b650883eba4a9d5f1b91b76495ba2aad3550c
carloseduardosx <carloseduardosx2015@gmail.com>
2017-08-28 18:35:33 -0300

    Create base webpack configuration
    - Replace still existing `React` usage by `preact`
    - Use `sass` css pre-processor
    - Use webpack-dashboard to show fine grained details about compilation

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,7 @@
 import { h, render } from 'preact';
-import './styles/index.css';
 import App from './preact/App';
 import registerServiceWorker from './registerServiceWorker';
+import './styles/index.scss';
 
 render(<App />, document.getElementById('root'));
 registerServiceWorker();
-
-// TODO Prepare project infrastructure
-// TODO Prepare project API usage
-// TODO Prepare project features

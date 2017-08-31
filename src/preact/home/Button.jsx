4e596802e7541bd42ca6a4a407b68bc7d5143d4c
carloseduardosx <carloseduardosx2015@gmail.com>
2017-08-31 19:13:45 -0300

    Add Button action and state control for SearchInput

--- src/preact/home/Button.jsx
+++ src/preact/home/Button.jsx
@@ -1,7 +1,11 @@
 import { h } from 'preact';
 
-const Button = ({ label }) => (
-  <button className="button--material" type="button">
+const Button = ({ label, onClick }) => (
+  <button
+    className="button--material"
+    type="button"
+    onClick={onClick}
+  >
     <span className="label__span">{label}</span>
   </button>
 );
--- src/preact/home/SearchInput.jsx
+++ src/preact/home/SearchInput.jsx
@@ -1,9 +1,9 @@
 import { h } from 'preact';
 
-const SearchInput = ({ placeholder }) => (
+const SearchInput = ({ placeholder, onInput }) => (
   <div className="search">
     <div className="search__div" >
-      <input className="search__input" type="text" required />
+      <input className="search__input" type="text" onInput={onInput} required />
       <span className="search--bar" />
       <label className="search__label">{placeholder}</label>
     </div>

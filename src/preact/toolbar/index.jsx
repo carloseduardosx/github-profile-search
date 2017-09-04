3a0c130cfcd963f6051c6b2bf867144bfa782332
carloseduardosx <carloseduardosx2015@gmail.com>
2017-09-03 23:55:08 -0300

    Avoid navigation when hasn't user stored

--- src/preact/toolbar/index.jsx
+++ src/preact/toolbar/index.jsx
@@ -1,16 +1,23 @@
 import { h } from 'preact';
+import { createStructuredSelector } from 'reselect';
+import { connect } from 'preact-redux';
+import { user } from '../../redux/modules';
 import ToolbarTitle from './ToolbarTitle';
 import ToolbarAction from './ToolbarAction';
 import ToolbarActionContainer from './ToolbarActionContainer';
 
-const Toolbar = () => (
+const Toolbar = ({ username }) => (
   <div className="toolbar__div">
     <ToolbarTitle label="GitHub Profile Search" />
     <ToolbarActionContainer>
-      <ToolbarAction path="/github-profile-search/repos" label="REPOS" />
-      <ToolbarAction path="/github-profile-search/notes" label="NOTES" />
+      <ToolbarAction path={username ? '/github-profile-search/repos' : ''} label="REPOS" />
+      <ToolbarAction path={username ? '/github-profile-search/notes' : ''} label="NOTES" />
     </ToolbarActionContainer>
   </div>
 );
 
-export default Toolbar;
+const mapStateToProps = createStructuredSelector({
+  username: user.selectors.getUsername
+});
+
+export default connect(mapStateToProps)(Toolbar);

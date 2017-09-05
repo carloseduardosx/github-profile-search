04553747286c7fe6ca9862b98b114ddfce84c10d
carloseduardosx <carloseduardosx2015@gmail.com>
2017-09-04 23:55:17 -0300

    Accebility changes

--- src/preact/home/SearchInput.jsx
+++ src/preact/home/SearchInput.jsx
@@ -3,9 +3,9 @@ import { h } from 'preact';
 const SearchInput = ({ placeholder, onInput, value }) => (
   <div className="search">
     <div className="search__div" >
-      <input className="search__input" type="text" value={value} onInput={onInput} required />
+      <input id="search__input" className="search__input" type="text" value={value} onInput={onInput} required />
       <span className="search--bar" />
-      <label className="search__label">{placeholder}</label>
+      <label className="search__label" htmlFor="search__input" >{placeholder}</label>
     </div>
   </div>
 );
